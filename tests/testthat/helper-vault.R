38e426779f7deb959e500df61813a1388fdb41b8
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-08-22 17:35:30 +0100

    Helpers for testing

--- tests/testthat/helper-vault.R
+++ tests/testthat/helper-vault.R
@@ -20,15 +20,23 @@ server_manager <- R6::R6Class(
       self$client <- client
     },
     start = function() {
+      vault <- Sys.which("vault")
+      if (!nzchar(vault)) {
+        stop("vault executable not found")
+      }
       message("starting server")
-      args <- c("server", paste0("-config=", config_path))
+      args <- c("server", paste0("-config=", self$config_path))
       self$process <-
         processx::process$new(vault, args, stdout = "|", stderr = "|")
 
       for (i in 1:20) {
-        res <- try(self$client$sys_initialized(), silent = TRUE)
+        res <- try(self$client$sys_is_initialized(), silent = TRUE)
         if (!inherits(res, "try-error")) {
           return(TRUE)
+        }
+        if (!self$process$is_alive()) {
+          err <- paste(self$process$read_all_output_lines(), collapse = "\n")
+          stop("vault has died: ", err)
         }
         message("...waiting for Vault to start")
         Sys.sleep(0.5)
@@ -46,5 +54,27 @@ server_manager <- R6::R6Class(
     },
     unseal = function() {
       self$client$unseal_multi(self$keys)
+    },
+    kill = function() {
+      self$process$kill()
     }
   ))
+
+test_client <- function(...) {
+  vault_client$new(verify = "server/server-cert.pem")
+}
+
+server <- NULL
+server_start <- function() {
+  server <<- server_manager$new("server", test_client())
+  server$start()
+  server$sys_initialize()
+  server$unseal()
+  invisible()
+}
+
+server_teardown <- function() {
+  if (server$process$is_alive()) {
+    server$process$kill()
+  }
+}
--- tests/testthat/server/README.md
+++ tests/testthat/server/README.md
@@ -1,3 +1,3 @@
 # Test certificates
 
-These certificates are used to test TLS authentication in HVAC. Do not reuse them for any other purposes.
+These certificates are used to test TLS authentication in vault. Do not reuse them for any other purposes.  They are generated as-needed by the `generate.sh` command.  This is the same approach for testing as used by [hvac](https://github.com/ianunruh/hvac), and this implementation follows theirs verbatim.
