227717b7addcab09547edab10c6f08a035fbc261
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2017-11-30 16:59:27 +0000

    Be more careful during tests

--- tests/testthat/test-aaa.R
+++ tests/testthat/test-aaa.R
@@ -1,3 +1,9 @@
+## Unset things that might be set by users and would interfere with
+## testing (or worse, cause the testing to interfere with their vault
+## instance).
+Sys.unsetenv("VAULTR_CACHE_DIR")
+Sys.unsetenv("VAULT_ADDR")
+
 if (identical(Sys.getenv("TRAVIS"), "true") &&
     identical(Sys.getenv("VAULTR_TEST_SERVER_INSTALL"), "true")) {
   vault_test_server_install()
