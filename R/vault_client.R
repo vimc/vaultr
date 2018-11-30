5269391000727c3ac6a08ce8ed8cb28be221e406
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-11-30 10:10:12 +0000

    More system methods

--- R/vault_client.R
+++ R/vault_client.R
@@ -411,6 +411,27 @@ R6_vault_client_operator <- R6::R6Class(
     is_initialized = function() {
       d <- private$api_client$GET("/sys/init", allow_missing_token = TRUE)
       d$initialized
+    },
+
+    init = function(secret_shares, secret_threshold) {
+      ## TODO: pgp not supported here
+      assert_scalar_integer(secret_shares)
+      assert_scalar_integer(secret_threshold)
+      body <- list(secret_shares = secret_shares,
+                   secret_threshold = secret_threshold)
+      res <- private$api_client$PUT("/sys/init", body = body,
+                                    allow_missing_token = TRUE)
+      res$keys <- list_to_character(res$keys)
+      res$keys_base64 <- list_to_character(res$keys_base64)
+      res
+    },
+
+    unseal = function(key, reset = FALSE) {
+      assert_scalar_character(key)
+      assert_scalar_logical(reset)
+      body <- list(key = key, reset = reset)
+      private$api_client$PUT("/sys/unseal", body = body,
+                             allow_missing_token = TRUE)
     }
   ))
 
