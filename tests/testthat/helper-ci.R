be6fa4fa13863647ba2847b8ed183b8232a5b8fd
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-06-08 16:35:43 +0100

    Try and fix the install logc

--- tests/testthat/helper-ci.R
+++ tests/testthat/helper-ci.R
@@ -1,7 +1,7 @@
 if (identical(Sys.getenv("CI"), "true") &&
     identical(Sys.getenv("NOT_CRAN"), "true") &&
     identical(Sys.getenv("VAULTR_TEST_SERVER_INSTALL"), "true") &&
-    !nzchar(Sys.getenv("GITHUB_WORKSPACE"))) {
+    nzchar(Sys.getenv("GITHUB_WORKSPACE"))) {
   Sys.setenv(VAULTR_TEST_SERVER_BIN_PATH =
                sprintf("%s/.vault", Sys.getenv("GITHUB_WORKSPACE")))
   vault_test_server_install()
