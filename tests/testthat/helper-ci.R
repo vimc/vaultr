b80d3d0ff9535cbfacfe8a30f1a14a02da662b1e
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-06-08 16:02:27 +0100

    Update to work with gha/new vault

--- tests/testthat/helper-ci.R
+++ tests/testthat/helper-ci.R
@@ -1,4 +1,5 @@
-if (identical(Sys.getenv("TRAVIS"), "true") &&
+if (identical(Sys.getenv("CI"), "true") &&
+    identical(Sys.getenv("NOT_CRAN"), "true") &&
     identical(Sys.getenv("VAULTR_TEST_SERVER_INSTALL"), "true")) {
   vault_test_server_install()
 }
