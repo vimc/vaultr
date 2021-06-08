7560e4d16d8a1edcaf21008c9598876825fa3b9b
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-06-08 16:21:33 +0100

    try again with envvar setting

--- tests/testthat/helper-ci.R
+++ tests/testthat/helper-ci.R
@@ -1,5 +1,8 @@
 if (identical(Sys.getenv("CI"), "true") &&
     identical(Sys.getenv("NOT_CRAN"), "true") &&
-    identical(Sys.getenv("VAULTR_TEST_SERVER_INSTALL"), "true")) {
+    identical(Sys.getenv("VAULTR_TEST_SERVER_INSTALL"), "true") &&
+    !nzchar(Sys.getenv("GITHUB_WORKSPACE"))) {
+  Sys.setenv(VAULTR_TEST_SERVER_BIN_PATH =
+               sprintf("%s/.vault", Sys.getenv("GITHUB_WORKSPACE")))
   vault_test_server_install()
 }
