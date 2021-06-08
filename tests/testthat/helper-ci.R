dc780fb3d756fb1656b7a5cdcd6c818ceeee1fe1
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-06-08 18:26:18 +0100

    Use same install approach as orderly

--- tests/testthat/helper-ci.R
+++ tests/testthat/helper-ci.R
@@ -1,8 +1,5 @@
 if (identical(Sys.getenv("CI"), "true") &&
     identical(Sys.getenv("NOT_CRAN"), "true") &&
-    identical(Sys.getenv("VAULTR_TEST_SERVER_INSTALL"), "true") &&
-    nzchar(Sys.getenv("GITHUB_WORKSPACE"))) {
-  Sys.setenv(VAULTR_TEST_SERVER_BIN_PATH =
-               sprintf("%s/.vault", Sys.getenv("GITHUB_WORKSPACE")))
+    identical(Sys.getenv("VAULTR_TEST_SERVER_INSTALL"), "true")) {
   vault_test_server_install()
 }
