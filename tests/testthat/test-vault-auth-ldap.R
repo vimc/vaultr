d482a4d8f0806569dbe9f7a3743b143f8dff25d0
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-08-17 09:13:51 +0100

    Quieter tests

--- tests/testthat/test-vault-auth-ldap.R
+++ tests/testthat/test-vault-auth-ldap.R
@@ -2,12 +2,12 @@
 ## setup here.
 
 test_that("custom mount", {
-  srv <- vault_test_server()
+  srv <- test_vault_test_server()
   cl <- srv$client()
 
   cl$auth$enable("ldap", path = "ldap2")
   ldap <- cl$auth$ldap$custom_mount("ldap2")
-  expect_is(ldap, "vault_client_auth_ldap")
+  expect_s3_class(ldap, "vault_client_auth_ldap")
 
   info <- ldap_configure(ldap)
   expect_equal(ldap$configuration()$binddn, info$configuration$binddn)
@@ -15,7 +15,7 @@ test_that("custom mount", {
 
 
 test_that("ldap set policy: group", {
-  srv <- vault_test_server()
+  srv <- test_vault_test_server()
   cl <- srv$client()
   cl$auth$enable("ldap")
   info <- ldap_configure(cl$auth$ldap)
@@ -27,7 +27,7 @@ test_that("ldap set policy: group", {
 
 
 test_that("ldap set policy: user", {
-  srv <- vault_test_server()
+  srv <- test_vault_test_server()
   cl <- srv$client()
   cl$auth$enable("ldap")
   info <- ldap_configure(cl$auth$ldap)
@@ -40,7 +40,7 @@ test_that("ldap set policy: user", {
 
 
 test_that("delete group", {
-  srv <- vault_test_server()
+  srv <- test_vault_test_server()
   cl <- srv$client()
   cl$auth$enable("ldap")
   info <- ldap_configure(cl$auth$ldap)
@@ -51,7 +51,7 @@ test_that("delete group", {
 
 
 test_that("delete user", {
-  srv <- vault_test_server()
+  srv <- test_vault_test_server()
   cl <- srv$client()
   cl$auth$enable("ldap")
   info <- ldap_configure(cl$auth$ldap)
@@ -62,7 +62,7 @@ test_that("delete user", {
 
 
 test_that("can use ldap", {
-  srv <- vault_test_server()
+  srv <- test_vault_test_server()
   cl <- srv$client()
   cl$auth$enable("ldap")
   d <- cl$auth$list()
@@ -80,12 +80,12 @@ test_that("can use ldap", {
   token <- result$client_token
 
   cl2 <- srv$client(login = FALSE)
-  cl2$login(token = token, use_cache = FALSE)
+  cl2$login(token = token, use_cache = FALSE, quiet = TRUE)
   expect_true("example" %in% cl2$token$lookup_self()$policies)
   cl2$write("secret/a/thing", list(a = 1))
 
   cl3 <- srv$client(login = FALSE)
-  cl3$login(method = "ldap", use_cache = FALSE,
+  cl3$login(method = "ldap", use_cache = FALSE, quiet = TRUE,
             username = info$username, password = info$password)
   expect_equal(cl3$read("secret/a/thing"), list(a = 1))
 })
